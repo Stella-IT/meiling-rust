9675ed739969134a3374e1670f55b2a1a56b8bff
Joosung Park <admin@slowmotion.dev>
2020-09-27 21:35:54 +0900

    feat: register new email address :sparkles:

--- src/gql/schema/objects.rs
+++ src/gql/schema/objects.rs
@@ -108,3 +108,51 @@ impl From<NewAuthenticationInfo> for model::NewAuthenticationInfo {
         }
     }
 }
+
+#[derive(GraphQLObject)]
+pub struct Email {
+    pub id: String,
+    pub address: String,
+    pub user_id: String,
+    pub registration_date: Option<NaiveDateTime>,
+    pub is_validated: bool,
+}
+
+impl TryFrom<model::Email> for Email {
+    type Error = Box<dyn Error>;
+
+    fn try_from(email: model::Email) -> Result<Self, Self::Error> {
+        Ok(Self {
+            id: uuid::Uuid::from_str(&String::from_utf8(email.id)?)?.to_string(),
+            address: email.address,
+            user_id: uuid::Uuid::from_str(&String::from_utf8(email.user_id)?)?.to_string(),
+            registration_date: email.registration_date,
+            is_validated: match email.is_validated {
+                0 => false,
+                _ => true,
+            },
+        })
+    }
+}
+
+#[derive(GraphQLInputObject)]
+pub struct NewEmail {
+    pub address: String,
+    pub user_id: String,
+    pub registration_date: Option<NaiveDateTime>,
+    pub is_validated: bool,
+}
+
+impl From<NewEmail> for model::NewEmail {
+    fn from(email: NewEmail) -> Self {
+        Self {
+            address: email.address,
+            user_id: email.user_id.into_bytes(),
+            registration_date: email.registration_date,
+            is_validated: match email.is_validated {
+                true => 1,
+                false => 0,
+            },
+        }
+    }
+}
--- src/meiling/mod.rs
+++ src/meiling/mod.rs
@@ -1,3 +1,4 @@
+use std::convert::TryFrom;
 use std::error::Error;
 use std::str::FromStr;
 
@@ -7,7 +8,6 @@ use uuid::Uuid;
 
 use crate::database::model;
 use crate::gql::schema as gql;
-use std::convert::TryFrom;
 
 pub fn create_user(
     conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
@@ -27,22 +27,23 @@ pub fn create_user(
     Ok(gql::objects::User::try_from(inserted_user)?)
 }
 
+fn get_user(
+    conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
+    uuid: &String,
+) -> Result<model::User, Box<dyn Error>> {
+    use crate::database::schema::user::dsl::*;
+    use diesel::prelude::*;
+
+    let binary_user_id = Uuid::from_str(uuid.as_str())?.to_string().into_bytes();
+
+    Ok(user.filter(id.eq(binary_user_id)).get_result(conn)?)
+}
+
 pub fn add_authentication_method(
     conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
     new_authentication_info: gql::objects::NewAuthenticationInfo,
 ) -> Result<Vec<gql::objects::AuthenticationInfo>, Box<dyn Error>> {
-    let user: model::User = {
-        use crate::database::schema::user::dsl::*;
-        use diesel::prelude::*;
-
-        let binary_user_id = Uuid::from_str(new_authentication_info.user_id.as_str())?
-            .to_string()
-            .into_bytes();
-
-        user.filter(id.eq(binary_user_id)).get_result(conn)?
-    };
-
-    println!("{:?}", &user);
+    let user: model::User = get_user(conn, &new_authentication_info.user_id)?;
 
     {
         use crate::database::schema::auth_info::dsl::*;
@@ -63,5 +64,33 @@ pub fn add_authentication_method(
     Ok(authentication_method
         .into_iter()
         .map(|info| gql::objects::AuthenticationInfo::try_from(info).unwrap())
+        .collect())
+}
+
+pub fn add_email(
+    conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
+    new_email: gql::objects::NewEmail,
+) -> Result<Vec<gql::objects::Email>, Box<dyn Error>> {
+    let user: model::User = get_user(conn, &new_email.user_id)?;
+
+    {
+        use crate::database::schema::email::dsl::*;
+        use diesel::prelude::*;
+
+        diesel::insert_into(email)
+            .values(model::NewEmail::from(new_email))
+            .execute(conn)?;
+    }
+
+    let emails: Vec<model::Email> = {
+        use crate::database::schema::email::dsl::*;
+        use diesel::prelude::*;
+
+        email.filter(user_id.eq(user.id)).get_results(conn)?
+    };
+
+    Ok(emails
+        .into_iter()
+        .map(|info| gql::objects::Email::try_from(info).unwrap())
         .collect())
 }
