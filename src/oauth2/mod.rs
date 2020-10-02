8f97f2c36d9b6472dd8a7d090f911d5ee8a1dd67
Alex4386 <psh010209@gmail.com>
2020-10-02 18:59:12 +0900

    feat: partial implementation of getting token from db

--- src/oauth2/mod.rs
+++ src/oauth2/mod.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, HttpRequest, HttpResponse, post, Responder};
+use actix_web::{get, HttpRequest, HttpResponse, post, Responder, web};
+use std::sync::Arc;
+use crate::gql::schema::Schema;
+use crate::gql::context::Context;
+use qstring::QString;
+use crate::database::model::Client;
+use std::error::Error;
 
 #[get("/auth")]
 pub async fn auth_redirect(req: HttpRequest) -> impl Responder {
@@ -9,10 +15,24 @@ pub async fn auth_redirect(req: HttpRequest) -> impl Responder {
 }
 
 #[get("/token")]
-pub async fn get_token() -> impl Responder {
-    HttpResponse::Ok().body("")
-}
+async fn get_token(
+    st: web::Data<Arc<Schema>>,
+    context: web::Data<Arc<Context>>,
+    req: HttpRequest,
+) -> Result<HttpResponse, Box<dyn Error>> {
+    use crate::database::schema::client::dsl::*;
+    use diesel::prelude::*;
 
-async fn is_client_valid(client_id: String, client_secret: String) {
+    let conn = context.database_pool.get()?;
 
+    let query_string = req.query_string();
+    let queries = QString::from(query_string);
+
+    let da_client = {
+        let client_id = queries.get("client_id");
+
+        client.filter(id.eq(client_id.into())).get_result(&conn)?
+    };
+
+    Ok(HttpResponse::Ok().body(da_client.id.into()))
 }
